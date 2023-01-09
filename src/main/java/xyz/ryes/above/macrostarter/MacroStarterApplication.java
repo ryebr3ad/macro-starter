0eb92adb11e1bf701ff73a0c1d2fb46d0af68e6a
ryebr3ad <rprauschenberg@gmail.com>
2023-01-08 20:05:48 -0500

    Initial commit

---
+++ src/main/java/xyz/ryes/above/macrostarter/MacroStarterApplication.java
@@ -0,0 +1,13 @@
+package xyz.ryes.above.macrostarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacroStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacroStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/ryes/above/macrostarter/MacroStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.ryes.above.macrostarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacroStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
